f4ff2c25009ab7f7dd07f1903d2e53374dc74c84
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-02 14:53:55 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,48 +1,18 @@
 def find_min_in_nested_arrays(src)
-  
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
-  lowest_temps_array = []
-  row_index = 0
-    while row_index < src.count
-      element_index = 0
-      while element_index < src[row_index].count
-        weather_temp = 0
-        if src[row_index][element_index] < weather_temp
-        binding.pry
-        weather_temp = src[row_index][element_index]
-        end
-        lowest_temps_array << weather_temp
-        element_index += 1
+  min_temps = []
+  for i in 0...src.length
+    min_value = src[i][0]
+    for f in 0...src[i].length
+      if min_value > src[i][f]
+        min_value = src[i][f]
+      end
     end
-    row_index += 1
+    min_temps.push(min_value)
   end
-  lowest_temps_array
+  p min_temps
 end
-
-
-
-
-
-
-
-
-
-# def find_min_in_nested_arrays(src)
-#   # src will be an array of arrays of integers
-#   # Produce a new Array that contains the smallest number of each of the nested arrays
-#   min_temps = []
-#   for i in 0...src.length
-#     min_value = src[i][0]
-#     for f in 0...src[i].length
-#       if min_value > src[i][f]
-#         min_value = src[i][f]
-#       end
-#     end
-#     min_temps.push(min_value)
-#   end
-#   p min_temps
-# end
 
 
 
